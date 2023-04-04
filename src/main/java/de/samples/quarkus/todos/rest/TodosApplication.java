484829c212e63e5933aefc7c8ff32e3911cfcc71
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2023-04-04 13:39:41 +0200

    add URL prefix 'api/v1' to REST API

---
+++ src/main/java/de/samples/quarkus/todos/rest/TodosApplication.java
@@ -0,0 +1,9 @@
+package de.samples.quarkus.todos.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api/v1")
+public class TodosApplication extends Application {
+
+}
