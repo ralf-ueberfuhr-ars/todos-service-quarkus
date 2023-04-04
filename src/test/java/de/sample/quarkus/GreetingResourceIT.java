a50f3d9056908bf05209cd20a581eaf2d109461a
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2023-04-04 11:07:35 +0200

    Disable tests

--- src/test/java/de/sample/quarkus/GreetingResourceIT.java
+++ src/test/java/de/sample/quarkus/GreetingResourceIT.java
@@ -1,7 +1,10 @@
 package de.sample.quarkus;
 
+import org.junit.jupiter.api.Disabled;
+
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
+@Disabled
 @QuarkusIntegrationTest
 public class GreetingResourceIT extends GreetingResourceTest {
     // Execute the same tests but in packaged mode.
--- src/test/java/de/sample/quarkus/GreetingResourceTest.java
+++ src/test/java/de/sample/quarkus/GreetingResourceTest.java
@@ -1,11 +1,14 @@
 package de.sample.quarkus;
 
 import io.quarkus.test.junit.QuarkusTest;
+
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
+@Disabled
 @QuarkusTest
 public class GreetingResourceTest {
 
